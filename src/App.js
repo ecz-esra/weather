8a7bb8192894fbb85f6c9638b996192afdc0fcba
ecz-esra <ecz.2023@gmail.com>
2022-12-16 21:30:39 +0000

    degisiklikler yapildi

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import City from './City';
 
 function App() {
-  const key ="122b990e398595bce8f25d0f3848ee16";
-  const[search,setSearch]=useState('');
-  
-  const[city,setCity]=useState();
-  useEffect(()=>{
-    async function getApi() {
-      try {
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`);
-        console.log(response);
-        setCity(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    getApi();
-  },[])
+  const key = '122b990e398595bce8f25d0f3848ee16';
+  const [searchParams, setSearchParams] = useState('');
+  const [weatherDetails, setWeatherDetails] = useState();
 
-    console.log(search);
+  function search() {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${searchParams}&appid=${key}&units=metric`
+      )
+      .then((res) => setWeatherDetails(res.data))
+      .catch((err) => console.log(err));
+  }
+
   return (
     <div className="App">
       <div>
-      
-  <input type="text" onChange={(event)=>setSearch(event.target.value)} 
+        
+       <div className="mb-3 pt-0">
+  <input type="text"
+  onChange={(event) => setSearchParams(event.target.value)}
    placeholder="Placeholder" 
-   className="px-3 py-4 w-{250px} placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-25"/>
-  <City city={city}/>
-  </div>
+   className="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-100%"/>
 </div>
-  
+
+        
+        <button onClick={() => search()}
+        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
+        Search
+      </button>
+        <City weatherDetails={weatherDetails} />
+      </div>
+    </div>
+    
   );
-  }
+}
 
 export default App;
--- src/City.jsx
+++ src/City.jsx
@@ -1,8 +1,13 @@
-import React from 'react' 
- const City=(city)=>{
-    console.log(city)
+import React from 'react';
+const City = (weatherDetails) => {
+  const data = weatherDetails.weatherDetails;
+  console.log(data);
   return (
-    <div>Hello</div>
-  )
+    <div>
+      <p>City:{data?.name}</p>
+      <p>Temperature:{data?.main?.temp}</p>
+      <p>Feels_like:{data?.main?.feels_like}</p>
+    </div>
+  );
 };
 export default City;
